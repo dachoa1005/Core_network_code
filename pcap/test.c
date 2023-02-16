19d14113cc4847370649f4a0f87d02f55bfe5cd1
dachoa1005 <hoa.pd185357@sis.hust.edu.vn>
2023-02-16 03:29:10 -0800

    update

--- pcap/test.c
+++ pcap/test.c
@@ -17,15 +17,21 @@ int main()
 	bpf_u_int32 net;
 
 	// Find default device 
-	dev = pcap_lookupdev(errbuf);
+	// pcap_findalldevs(&dev, errbuf);
+	dev = pcap_lookupdev(errbuf); 
+	// find a default device on which to capture, errbuf used to return error message
+	// pcap_lookupdev() returns a non null pointer to a string containing the name of the device on which to capture
+	// if it fails, it returns a null pointer and the error message is stored in errbuf
+
 	if (dev == NULL) {
 		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
 		return(2);
 	}
 	printf("Device: %s\n", dev);
 
-	// Step 1: Open live pcap session on NIC with name enp0s3
+	// Step 1: Open live pcap session on NIC with name dev
 	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	// obtain a 
 	if (handle == NULL){
 		fprintf(stderr, "Couldn't  open device %s: %s\n", dev, errbuf);
 		return(2);	
